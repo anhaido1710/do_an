5cf4ca56777376b50471343f4c33a0906ad0c92c
anhaido1710 <66657388+anhaido1710@users.noreply.github.com>
2020-06-11 23:07:11 +0700

    Create hien_thi_chi_tiet_san_pham.php

---
+++ views/hien_thi_chi_tiet_san_pham.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>chi tiết sp</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="left">
+		
+	</div>
+	<div class="right">
+		
+	</div>
+</body>
+</html>
