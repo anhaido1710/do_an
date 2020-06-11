f93f829c9dec3c7e7211cd57aedd7bbde8e7b4bf
anhaido1710 <66657388+anhaido1710@users.noreply.github.com>
2020-06-11 23:05:10 +0700

    Create hien_thi_san_pham.php

---
+++ views/hien_thi_san_pham.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>hien thi san pham</title>
+</head>
+<body>
+	<div class="all-product">
+	</div>
+	<div class="end-product">
+		<div class="tittle-end">
+			SHIRTS FOR MEN
+		</div>
+		<div class="detail-end">
+			Từ giản dị đến trang trọng , công phu đến sánh đôi, áo sơ mi dành cho nam giới phục vụ cho những người đàn ông lựa chọn tủ quần áo phối hợp. Bao gồm denim , vải lanh , sọc và hoa , một vài tên, bộ sưu tập mang đến những thiết kế vượt thời gian để lấp đầy những khoảng trống trong tiết mục của bạn.
+		</div>
+	</div>
+</body>
+</html>
