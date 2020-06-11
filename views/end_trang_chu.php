e86f4157404bd7778a32135003f229c44683ed20
anhaido1710 <66657388+anhaido1710@users.noreply.github.com>
2020-06-11 23:04:24 +0700

    Create end_trang_chu.php

---
+++ views/end_trang_chu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="views/css/end_trang_chu.css">
+</head>
+<body>
+	<div class="end-all">
+		<div class="join">
+			<div class="name-join">
+				JOIN OUR NEWSLETTER
+			</div>
+			<div class="input-join">
+				<input type="text" id="email" placeholder="Vui lòng nhập địa chỉ email tại đây">
+			</div>
+		</div>
+		<div class="join1">
+			<ul>
+				<li>INSTAGRAM</li>
+				<li>FACEBOOK</li>
+				<li>TWITTER</li>
+				<li>PINTEREST</li>
+				<li>YOUTUBE</li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
