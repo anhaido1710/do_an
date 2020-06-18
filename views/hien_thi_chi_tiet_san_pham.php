51bdee7c22c8c95d95be3774a057c7344125627d
anhaido1710 <66657388+anhaido1710@users.noreply.github.com>
2020-06-18 21:58:58 +0700

    Update hien_thi_chi_tiet_san_pham.php

--- views/hien_thi_chi_tiet_san_pham.php
+++ views/hien_thi_chi_tiet_san_pham.php
@@ -4,14 +4,21 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>chi tiết sp</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="views/css/hien_thi_chi_tiet_sp.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="model/JS/hien_thi_chi_tiet_sp.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="views/css/header.css">
+	<script src="https://kit.fontawesome.com/de7000759e.js" crossorigin="anonymous"></script>
 </head>
 <body>
+	<?php include "views/header.html"; ?>
 	<div class="left">
 		
 	</div>
 	<div class="right">
-		
+		<div class="information">
+			
+		</div>
 	</div>
 </body>
 </html>
