83e4c3aad97e15fd34b4980b7129ce615f4f939d
anhai1710 <65010396+anhai1710@users.noreply.github.com>
2020-07-03 14:54:19 +0700

    cr

---
+++ views/ad_min.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="views/css/admin.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="model/JS/admin_sp_danh_muc.js" type="text/javascript"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+</head>
+<body style="font-size: 17px; font-family: tahoma;">
+    
+        <div class="left-menu">
+        	<div class="icon">
+        		<h2>SSStutter</h2>
+        	</div>
+        	<div class="menu">
+            <ul>
+                <li id="san_pham"><p href="">SẢN PHẨM</p></li>
+                <div class="danh_muc_admin"><?php include "views/admin_control_sp.php" ?></div>
+                <li id="thong_tin"><p href="">THÔNG TIN</p></li>
+                <li id="bai_dang"><p href="">BÀI ĐĂNG</p></li>
+                
+            </ul>
+            </div>
+        </div>
+        <div class="admin">
+          
+        </div>
+   		<div class="admin-right-menu">
+   			<div class="admin-right-tittle">
+   				
+   			</div>
+   			<div class="admin-right-content">
+   				<div class="admin-content-detail">
+   					
+   				</div>
+   				<div class="page-detail">
+   					
+   				</div>
+   				<div class="update-page">
+   				
+   				</div>
+   			</div>
+
+   		</div>
+    
+    
+</body>
+</html>
